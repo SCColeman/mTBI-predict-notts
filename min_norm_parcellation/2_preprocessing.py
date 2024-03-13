b1535d8a90f7c2e5e52c5fc73b4aadb6aa9bba9f
SCColeman <ppysc6@nottingham.ac.uk>
2024-03-13 17:23:14 +0000

    Inclusion of empty room noise
    Empty room is now loaded in the pre-processing script and used in the source reconstruction for pre-whitening, this *hugely* improves min-norm estimates.

--- min_norm_parcellation/2_preprocessing.py
+++ min_norm_parcellation/2_preprocessing.py
@@ -5,13 +5,10 @@ Preprocess CRT data, taken on CTF MEG system. Can easily be automated.
 @author: Sebastian C. Coleman, ppysc6@nottingham.ac.uk
 """
 
-import os
 import os.path as op
-
 import numpy as np
 import mne
-from mne_bids import BIDSPath, read_raw_bids, inspect_dataset
-from matplotlib import pyplot as plt
+from mne_bids import BIDSPath, read_raw_bids
 
 ### need the following line so 3d plotting works, for some reason
 mne.viz.set_3d_options(depth_peeling=False, antialias=False)
@@ -19,14 +16,15 @@ mne.viz.set_3d_options(depth_peeling=False, antialias=False)
 #%% set up BIDS path
 
 bids_root = r'R:\DRS-mTBI\Seb\mTBI_predict\BIDS'
-deriv_root = r'R:\DRS-PSR\Seb\mTBI_testing\derivatives'
+deriv_root = r'R:\DRS-mTBI\Seb\mTBI_predict\derivatives'
 
 # scanning session info
-subject = '2003'
+subject = '2001'
 session = '03N'
 task = 'CRT'  # name of the task
 run = '01'
 suffix = 'meg'
+date = '20230620'   # for empty room noise (needed for minimum-norm estimate)
 
 bids_path = BIDSPath(subject=subject, session=session,
 task=task, run=run, suffix=suffix, root=bids_root)
@@ -42,6 +40,7 @@ if not op.exists(deriv_path.directory):
 data = read_raw_bids(bids_path=bids_path, verbose=False)
 data_raw = data.copy()  # these lines allow for easier post-hoc debugging
 print(data.info)
+sfreq = data.info["sfreq"]
 
 #%% load triggers
 
@@ -56,24 +55,62 @@ mne.viz.plot_events(events)
 data.apply_gradient_compensation(grade=3)
 data_3rd = data.copy()
 
-#%% downsample
-
-orig_freq = 600
-sfreq = 250
-data.resample(sfreq=sfreq)
-data_ds = data.copy()
-
-### downsample events to match
-events[:,0] = np.round(events[:,0] * (sfreq/orig_freq))
-
 #%% broadband filter
 
-data.filter(l_freq=1, h_freq=45)
+data.load_data().filter(l_freq=1, h_freq=45)
 data_bband = data.copy()
+
+#%% get movement parameters and annotate high movement
+
+# get hpi info
+chpi_locs = mne.chpi.extract_chpi_locs_ctf(data, verbose=False)
+head_pos = mne.chpi.compute_head_pos(data.info, chpi_locs, verbose=False)
+head_movement_fig = mne.viz.plot_head_positions(head_pos, mode="traces")
+
+# calculate gradient of head movement
+head_pos_grad = head_pos.copy()
+head_pos_grad[:,1:] = np.gradient(head_pos[:,1:], axis=0)
+head_movement_fig = mne.viz.plot_head_positions(head_pos_grad, mode="traces")
+
+# mark bad head movements greater than 2mm / s
+bad_head_bool = head_pos_grad[:,1:4] > 0.001
+bad_head_bool = np.sum(bad_head_bool, axis=1) > 0
+
+# create annotation
+hpi_sfreq = head_pos[1,0] - head_pos[0,0]
+bad_head_duration = 5*hpi_sfreq
+bad_head_onset = head_pos[bad_head_bool,0] - 2*hpi_sfreq
+bad_head_description = "BAD_head"
+bad_head_annot = mne.Annotations(bad_head_onset, bad_head_duration, 
+                                 bad_head_description, 
+                                 orig_time=data.info['meas_date'])
+
+#%% mark SQUID resets
+
+squid_annot, bad_chan = mne.preprocessing.annotate_amplitude(
+                                    data, peak=dict(mag=2e-12), picks='meg',
+                                    bad_percent=5, min_duration=0.005)
+squid_annot.onset = squid_annot.onset - 2
+squid_annot.duration = [4] * len(squid_annot.duration)
+data.info["bads"].extend(bad_chan)
+
+#%% annotate smaller muscle artefacts etc (DONT USE ZSCORE HERE)
+
+muscle_annot, bad_chan = mne.preprocessing.annotate_amplitude(
+                                    data, peak=dict(mag=5e-13), picks='meg',
+                                    bad_percent=5, min_duration=1/600)
+muscle_annot.onset = muscle_annot.onset - 0.2
+muscle_annot.duration = muscle_annot.duration + 0.4
+
+#%% 
+
+data.set_annotations(bad_head_annot + squid_annot + muscle_annot)
+data.plot()
 
 #%% plot psd
 
 data.plot_psd(fmax=45, picks='mag').show()
+#data.plot()
 
 #%% Automated ICA removal
 
@@ -82,27 +119,45 @@ ECG_channels = ["UADC009-4123"]
 blink_channels = ['MLT31-4123', 'MRT31-4123'];
 
 ica = mne.preprocessing.ICA(n_components=30)
-ica.fit(data.copy().pick_types(meg=True))
+ica.fit(data.copy().pick_types(meg=True), reject_by_annotation=True)
 
-eog_indices, eog_scores = ica.find_bads_eog(data, EOG_channels[0])
-blink_indices, blink_scores = ica.find_bads_eog(data, blink_channels)
-ecg_indices, ecg_scores = ica.find_bads_ecg(data, ECG_channels[0])
+eog_indices, eog_scores = ica.find_bads_eog(data, EOG_channels[0],
+                                            reject_by_annotation=True)
+blink_indices, blink_scores = ica.find_bads_eog(data, blink_channels,
+                                                reject_by_annotation=True)
+ecg_indices, ecg_scores = ica.find_bads_ecg(data, ECG_channels[0],
+                                            reject_by_annotation=True)
 
 ica.exclude = list(dict.fromkeys(eog_indices + blink_indices + ecg_indices))
 
 # plot diagnostics
 ica.plot_properties(data, picks=ica.exclude)
 
+ica.plot_sources(data)
+
 # apply
 ica.apply(data)
 data_ica = data.copy()
+
+#%% preprocess empty room
+
+noise_fname = op.join(bids_root, 'sub-emptyroom', 'ses-' + date, 'meg', 
+                      'sub-emptyroom_ses-' + date + '_task-noise_meg.fif')
+noise_raw = mne.io.Raw(noise_fname)
+
+noise_raw.apply_gradient_compensation(grade=3)
+noise_raw.load_data().filter(l_freq=1, h_freq=45)
+noise_raw.info['bads'] = data.info['bads']
+ica.apply(noise_raw)
 
 #%% save out preprocessed data
 
 preproc_fname = deriv_path.basename + "-raw.fif"
 events_fname = deriv_path.basename + "-events.fif"
+noise_fname = deriv_path.basename + "-noise.fif"
 data.save(op.join(deriv_path.directory, preproc_fname), overwrite=True)
 mne.write_events(op.join(deriv_path.directory, events_fname), events, overwrite=True)
+noise_raw.save(op.join(deriv_path.directory, noise_fname), overwrite=True)
 
 
 
--- min_norm_parcellation/3_extract_source_epochs.py
+++ min_norm_parcellation/3_extract_source_epochs.py
@@ -19,11 +19,11 @@ mne.viz.set_3d_options(depth_peeling=False, antialias=False)
 #%% set up BIDS path
 
 bids_root = r'R:\DRS-mTBI\Seb\mTBI_predict\BIDS'
-deriv_root = r'R:\DRS-PSR\Seb\mTBI_testing\derivatives'
+deriv_root = r'R:\DRS-mTBI\Seb\mTBI_predict\derivatives'
 
 # scanning session info
-subject = '2003'
-session = '05N'
+subject = '2001'
+session = '03N'
 task = 'CRT'  # name of the task
 run = '01'
 suffix = 'meg'
@@ -37,16 +37,23 @@ task=task, run=run, suffix=suffix, root=deriv_root)
 #%% load in data
 
 data = mne.io.Raw(op.join(deriv_path.directory, deriv_path.basename + "-raw.fif"))
+noise = mne.io.Raw(op.join(deriv_path.directory, deriv_path.basename + "-noise.fif"))
 data.pick("mag")
+noise.pick("mag")
 events = mne.read_events(op.join(deriv_path.directory, deriv_path.basename + "-events.fif"))
 src = op.join(deriv_path.directory, deriv_path.basename + "-src.fif")
 bem = op.join(deriv_path.directory, deriv_path.basename + "-bem.fif")
 trans = op.join(deriv_path.directory, deriv_path.basename + "-trans.fif")
 sfreq = data.info["sfreq"]
 
+#%% calculate noise covariance
+
+noise_cov = mne.compute_raw_covariance(noise)
+noise_cov.plot(data.info)
+
 #%% epoch based on trigger
 
-event_id = [101, 102] #[1, 32]   # trigger of interest, [1 31] -> btn press, [101, 102] -> stim
+event_id = [101, 102]  # trigger of interest
 tmin, tmax = -0.8, 1.2
 epochs = mne.Epochs(
     data,
@@ -54,22 +61,16 @@ epochs = mne.Epochs(
     event_id,
     tmin,
     tmax,
-    baseline=None,
+    baseline=(-0.4, -0.1),
     preload=True,
     reject=dict(mag=4e-12),
     reject_by_annotation=True)
-
-#%% compute covariance
-
-cov = mne.compute_covariance(epochs)
 
 #%% make forward model and inverse from files
 
 src = mne.read_source_spaces(src)
 fwd = mne.make_forward_solution(data.info, trans, src, bem, verbose=True)
-del src
-inv = make_inverse_operator(data.info, fwd, cov)
-del fwd
+inv = make_inverse_operator(data.info, fwd, noise_cov)
 
 #%% parcellation beamformer
 
@@ -80,8 +81,8 @@ parc = "aparc"
 labels = mne.read_labels_from_annot(fs_subject, parc=parc, subjects_dir=subjects_dir)
 
 stcs = apply_inverse_epochs(
-    epochs, inv, lambda2=1.0 / 9.0, pick_ori="normal", return_generator=True,
-    method="MNE"
+    epochs, inv, lambda2=2, pick_ori="normal", return_generator=True,
+    method="eLORETA"
 )
 
 label_ts = mne.extract_label_time_course(
